3161ce232da631382e483b18bb801e43912d49d3
oatsoda <andrew+github@milne.tel>
2020-11-24 09:33:18 +0000

    Project outline

---
+++ Quizzy.WebApp/Data/CosmosDbStartupExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Fluent;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Quizzy.WebApp.Data
+{
+    public static class CosmosDbStartupExtensions
+    {
+        public const string QUIZZES_DB_NAME = "Quizzes";
+        public const string QUIZZES_CONTAINER_NAME = "Quizzes";
+
+        public static IServiceCollection AddCosmosDb(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            var connString = configuration.GetConnectionString("CosmosQuizDb");
+            var cosmosClientBuilder = new CosmosClientBuilder(connString);
+            serviceCollection.AddSingleton(cosmosClientBuilder.Build());
+
+            return serviceCollection;
+        }
+
+        public static void ConfigureCosmosDbDevelopmentEnvironment(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            
+            var cosmosClient = scope.ServiceProvider.GetService<CosmosClient>();
+            var database = cosmosClient.CreateDatabaseIfNotExistsAsync(QUIZZES_DB_NAME, 400).GetAwaiter().GetResult();
+            database.Database.CreateContainerIfNotExistsAsync(new ContainerProperties(QUIZZES_CONTAINER_NAME, "/partkey"));
+        }
+    }
+}
