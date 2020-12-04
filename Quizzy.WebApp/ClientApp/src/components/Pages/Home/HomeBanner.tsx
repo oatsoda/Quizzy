3469b1bba7ef624a1f7d52a969f8d07e6068c710
oatsoda <andrew+github@milne.tel>
2020-12-04 09:37:10 +0000

    Home page tweaks

--- Quizzy.WebApp/ClientApp/src/components/Pages/Home/HomeBanner.tsx
+++ Quizzy.WebApp/ClientApp/src/components/Pages/Home/HomeBanner.tsx
@@ -6,7 +6,7 @@ export function HomeBanner() {
   return (
     <Jumbotron fluid={true} className="bg-dark text-white">
       <Container>          
-        <h1 className="display-4">Quizzy</h1>
+        <h1 className="display-3">Quizzy</h1>
         <p className="lead">Create and play interactive quizzes with family and friends.</p>
       </Container>
     </Jumbotron>
--- Quizzy.WebApp/ClientApp/src/components/Pages/Home/PageHome.tsx
+++ Quizzy.WebApp/ClientApp/src/components/Pages/Home/PageHome.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Col, Row } from 'reactstrap';
+import { Container, Col, Row, ListGroup, ListGroupItem } from 'reactstrap';
 import { CreateQuizLink } from './CreateQuizLink';
 import { HomeBanner } from './HomeBanner';
 import { JoinQuiz } from './JoinQuiz';
@@ -19,6 +19,14 @@ export function PageHome() {
           <JoinQuiz />
         </Col>
       </Row>
+      <h1 className="display-4 mt-5">How it works</h1>
+      <ListGroup>        
+        <ListGroupItem>1. Create a quiz with questions of your own.</ListGroupItem>
+        <ListGroupItem>2. Get your participants together on a video call and give them the code.</ListGroupItem>
+        <ListGroupItem>3. Once everyone is ready, start the quiz.</ListGroupItem>
+        <ListGroupItem>4. Participants use their own web browser to see and answer the questions.</ListGroupItem>
+        <ListGroupItem>5. Everyone can see who is connected and when they have answered each question.</ListGroupItem>
+      </ListGroup>
     </Container>
     </>
   );
