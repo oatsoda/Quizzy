d0e43ed18c95ee4d91b990698552240e4f77fef1
oatsoda <andrew+github@milne.tel>
2020-12-04 12:22:08 +0000

    Sticky footer.

--- Quizzy.WebApp/ClientApp/src/Layout.tsx
+++ Quizzy.WebApp/ClientApp/src/Layout.tsx
@@ -6,10 +6,17 @@ export class Layout extends Component {
 
   render () {
     return (
-      <div>
-        <Container fluid={true} className="p-0">
-          {this.props.children}
-        </Container>
+      <div className="d-flex flex-column min-vh-100">
+        <main className="flex-fill">
+          <Container fluid={true} className="p-0">
+            {this.props.children}
+          </Container>
+        </main>
+        <footer>
+          <Container className="text-center">
+            <p className="text-muted">Credits | About</p>
+          </Container>
+        </footer>
       </div>
     );
   }
