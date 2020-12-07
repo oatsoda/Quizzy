3de0b0263a2dcb89e17157b373da9862b52f45b8
oatsoda <andrew+github@milne.tel>
2020-12-07 14:43:36 +0000

    Add delete function. Add styling.

--- Quizzy.WebApp/ClientApp/src/components/Pages/Manage/CreateQuiz.tsx
+++ Quizzy.WebApp/ClientApp/src/components/Pages/Manage/CreateQuiz.tsx
@@ -1,5 +1,5 @@
-import React, { ChangeEvent, Dispatch, SetStateAction, useCallback, useState } from 'react';
-import { Button, CustomInput, Form, FormGroup, Input, Label } from 'reactstrap';
+import React, { ChangeEvent, Dispatch, SetStateAction, useCallback, useEffect, useState } from 'react';
+import { Button, Col, CustomInput, Form, FormGroup, Input, InputGroup, InputGroupAddon, InputGroupText, Label, Row } from 'reactstrap';
 import quizzesApi from '../../../api/quizzesApi';
 import { QuizNew, createQuizNew, QuizQuestionNew, createQuizQuestionNew } from '../../../api/quizTypes';
 import { ErrorDisplay } from '../../General/ErrorDisplay';
@@ -49,39 +49,66 @@ export function CreateQuiz() {
     },
     []
   );
+  
+  const removeQuestion = useCallback(
+    (i: number) => {
+      console.log(`delete ${i}`)
+      setNewQuizQuestions(prev => [...prev.slice(0,i), ...prev.slice(i+1)]);
+    },
+    []
+  );
 
   return (
     <Form>
       <Loader isLoading={isLoading} />
       <ErrorDisplay errorMessage={errorMessage} />
-      <FormGroup>
-        <Label for="name">Quiz Name</Label>
-        <Input type="text" name="name" placeholder="Enter name" onChange={handleInputChange} />
-      </FormGroup>
-      <FormGroup>
-        <Label for="creatorName">Your Name</Label>
-        <Input type="text" name="creatorName" placeholder="Enter your name" onChange={handleInputChange} />
-      </FormGroup>
-      <FormGroup>
-        <Label for="creatorEmail">Your Email</Label>
-        <Input type="email" name="creatorEmail" placeholder="Enter your email" onChange={handleInputChange} />
-      </FormGroup>
+      <Row form>
+        <Col lg={6}>
+          <FormGroup>
+            <Label for="name">Quiz Name</Label>
+            <Input type="text" name="name" placeholder="Enter name" onChange={handleInputChange} />
+          </FormGroup>
+        </Col>
+      </Row>
+      <Row form>
+        <Col lg={4}>
+          <FormGroup>
+            <Label for="creatorName">Your Name</Label>
+            <Input type="text" name="creatorName" placeholder="Enter your name" onChange={handleInputChange} />
+          </FormGroup>
+        </Col>
+        <Col>
+          <FormGroup>
+            <Label for="creatorEmail">Your Email</Label>
+            <InputGroup>
+              <InputGroupAddon addonType="prepend" id="emailprepend">
+                <InputGroupText>@</InputGroupText>
+              </InputGroupAddon>
+              <Input type="email" name="creatorEmail" placeholder="Enter your email" onChange={handleInputChange} />
+            </InputGroup>
+          </FormGroup>
+        </Col>
+      </Row>
       { newQuizQuestions.map((_, i) => (
-        <QuestionInput key={i} questionIndex={i} questions={newQuizQuestions} setQuestions={setNewQuizQuestions} />)
+        <QuestionInput key={i} questionIndex={i} questions={newQuizQuestions} setQuestions={setNewQuizQuestions} onRemove={removeQuestion} />)
         ) }
       <FormGroup>
         <Button color="secondary" onClick={addQuestion}>Add Question</Button>
       </FormGroup>
       <FormGroup>
-        <Button color="primary" onClick={saveNewQuiz}>Create</Button>
+        <Button color="primary" onClick={saveNewQuiz}>Create Quiz</Button>
       </FormGroup>
     </Form>
   );
 }
 
-function QuestionInput(props: { questionIndex: number, questions: QuizQuestionNew[], setQuestions: Dispatch<SetStateAction<QuizQuestionNew[]>> }) {
-  const { questionIndex, questions, setQuestions } = props;
+function QuestionInput(props: { questionIndex: number, questions: QuizQuestionNew[], setQuestions: Dispatch<SetStateAction<QuizQuestionNew[]>>, onRemove: (i: number) => void }) {
+  const { questionIndex, questions, setQuestions, onRemove } = props;
   const [question, setQuestion] = useState(questions[questionIndex]);
+
+  useEffect(() => {
+    setQuestion(questions[questionIndex]);
+  }, [questions, questionIndex])
 
   const handleInputChange = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
@@ -109,22 +136,40 @@ function QuestionInput(props: { questionIndex: number, questions: QuizQuestionNe
   }
 
   return (
-    <FormGroup>
-      <Label for="q">Question {questionIndex+1}</Label>
-      <Input type="text" name="q" value={question.q} placeholder="Question" onChange={handleInputChange} />
-      <Input type="text" name="a1" value={question.a1} placeholder="Answer 1" onChange={handleInputChange} />
-      <Input type="text" name="a2" value={question.a2} placeholder="Answer 2" onChange={handleInputChange} />
-      <Input type="text" name="a3" value={question.a3} placeholder="Answer 3" onChange={handleInputChange} />
-      <Input type="text" name="a4" value={question.a4} placeholder="Answer 4" onChange={handleInputChange} />
-      <CustomInput type="select" name="correctA" id="correctA" value={question.correctA ?? ""} onChange={handleInputChange}>
-        <option value="">Select Correct Answer</option>
-        <option value={1}>{answerDisplay(question.a1, 1)}</option>
-        <option value={2}>{answerDisplay(question.a2, 2)}</option>
-        <option value={3}>{answerDisplay(question.a3, 3)}</option>
-        <option value={4}>{answerDisplay(question.a4, 4)}</option>
-      </CustomInput>
-
-    </FormGroup>
+    <>
+    <Row form>
+      <Col>
+        <FormGroup>
+          <Label for="q" className="font-weight-bold">Question {questionIndex+1}</Label>
+          <Input type="text" name="q" value={question.q} placeholder="Question" onChange={handleInputChange} />
+        </FormGroup>
+      </Col>
+    </Row>
+    <div className="pl-3 mb-3 border-left">
+      <Row form className="mb-1">
+        <Col lg={6}><Input type="text" name="a1" value={question.a1} placeholder="Answer 1" onChange={handleInputChange} className="A" /></Col>
+        <Col lg={6}><Input type="text" name="a2" value={question.a2} placeholder="Answer 2" onChange={handleInputChange} className="B" /></Col>
+      </Row>
+      <Row form className="mb-1">
+        <Col lg={6}><Input type="text" name="a3" value={question.a3} placeholder="Answer 3" onChange={handleInputChange} className="C" /></Col>
+        <Col lg={6}><Input type="text" name="a4" value={question.a4} placeholder="Answer 4" onChange={handleInputChange} className="D" /></Col>
+      </Row>
+      <Row form>
+        <Col lg={4}>
+          <CustomInput type="select" name="correctA" id="correctA" value={question.correctA ?? ""} onChange={handleInputChange}>
+            <option value="">Select Correct Answer</option>
+            <option value={1}>{answerDisplay(question.a1, 1)}</option>
+            <option value={2}>{answerDisplay(question.a2, 2)}</option>
+            <option value={3}>{answerDisplay(question.a3, 3)}</option>
+            <option value={4}>{answerDisplay(question.a4, 4)}</option>
+          </CustomInput>
+        </Col>
+        { questionIndex > 0 && 
+          <Col><Button color="link" onClick={() => onRemove(questionIndex)}>Delete Question</Button></Col>
+        }
+      </Row>
+    </div>
+    </>
   );
 }
 
